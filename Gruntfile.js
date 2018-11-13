d5349d52e2bb285ecfe3eb2259683baf494617a9
jmy569999675 <569999675@qq.com>
2018-11-13 16:27:08 +0800

    删除没用的文件

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function(grunt){
             },
             dist:{
                 src:'src/js/*.js',
-                dest:'dist/<%=pkg.name%><%pkg.version%>.min.js'
+                dest:'dist/js/<%=pkg.name%><%pkg.version%>.min.js'
             }
         },
         cssmin:{
@@ -32,7 +32,7 @@ module.exports = function(grunt){
         	}
         },
         clean:{
-        	dest:['dist/*'],
+        	dest:['dist/*','sample/js/*'],
 
         },
         jshint:{
@@ -42,20 +42,26 @@ module.exports = function(grunt){
         	}
         },
         copy: {
-		    main:{
-		        expand: true, 
-		        cwd: 'src/',
-		        src: '*.js',
-		        dest: 'dist/'
-		    }
-		}
+		    js:{expand:true,cwd:'dist/js/',src:'*.min.js',dest:'sample/js/'}
+		},
+        replace:{
+            example:{
+                src:['sample/js/index.html'],
+                overwrite:true,
+                replace:[{
+                    from:/.min.js/g,
+                    to:'<%=pkg.name%><%pkg.version%>.min.js'
+                }]
+            }
+        }
 	});
 	// 告诉grunt需要使用插件
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-text-replace');
 	// 告诉grunt当我们在终端中输入grunt时需要做什么
-	grunt.registerTask('default',['jshint','clean','uglify']);
+	grunt.registerTask('default',['jshint','clean','uglify','copy']);
 };
