1f676117d768f5122495a419e72bba8ad815b033
jmy569999675 <569999675@qq.com>
2018-11-13 10:07:40 +0800

    添加移动端支持

--- src/js/wipe.js
+++ src/js/wipe.js
@@ -6,7 +6,54 @@ var radius = 20;//涂抹半径
 var lock = false;//表示鼠标状态,false为未按下,true为按下,
 var posY;
 var posX;
+// device保存设备类型,如果是移动端则为true,PC端为false
+var device = (/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent.toLowerCase()));
+// console.log(device)
+var down = device?"touchstart":"mousedown";
+var move = device?"touchmove":"mousemove";
+var end = device?"touchend":"mouseup";
 
+// 在canvas画布上监听自定义事件"mousedown",调用drawPoint函数
+cas.addEventListener(down,function(ev){
+	var evt = ev || window.event;
+	// 获取鼠标在视口的坐标,传递参数到drawPoint
+	posY = device ? evt.touches[0].clientY: evt.clientY;
+	posX = device ? evt.touches[0].clientX: evt.clientX;
+	drawPoint(context,posY,posX);
+	// drawAll(context,posX,posY);
+	lock=true;
+},false);
+
+
+// 监听手指移动事件
+cas.addEventListener(move,function(ev){
+	if (lock===true) {
+		var evt = ev||window.event;
+		// 获取鼠标在视口的坐标,传递参数到drawPoint
+		evt.preventDefault();
+		var moveX = device ? evt.touches[0].clientX :evt.clientX;
+		var moveY= device ? evt.touches[0].clientY : evt.clientY;
+		
+		drawLine(context,posX,posY,moveX,moveY);
+		// drawAll(context,posX,posY,moveX,moveY);
+		// 每一次的结束点变成下一次开始的点
+		posX = moveX;
+		posY = moveY;
+
+	}else{
+		return false;
+	}
+},false);
+
+// 监听手指离开屏幕
+cas.addEventListener(end,function(ev){
+	lock=false;
+	// 透明大于50的时候,清除画布中的内容
+	if(getTransparencyPercent(context)>=50){
+		console.log("大于50%面积");
+		clearRect(context);
+	}
+},false);
 
 function drawMask(context){
 	context.beginPath();
@@ -22,71 +69,52 @@ function drawPoint(context,posY,posX){
 	context.beginPath();
 	context.arc(posX,posY,radius,0,2*Math.PI);
 	context.globalCompositeOperation ="destination-out";
-	context.fillStyle="red";
+	// context.fillStyle="white";
 	context.fill();
 	context.save();
 	context.stroke();
 	// 恢复原有绘图状态
 	context.restore();
 }
-
-// 在canvas画布上监听自定义事件"mousedown",调用drawPoint函数
-cas.addEventListener("mousedown",function(ev){
-	var evt = ev || window.event;
-	// 获取鼠标在视口的坐标,传递参数到drawPoint
-	posY = evt.clientY;
-	posX = evt.clientX;
-	drawPoint(context,posY,posX);
-	lock=true;
-},false);
-
-cas.addEventListener("mouseup",function(evt){
-	lock=false;
-	// 透明大于50的时候,清除画布中的内容
-	if(getTransparencyPercent(context)>=50){
-		console.log("大于50%面积");
-		clearRect(context);
-	}
-	
-});
-// 清除画布
-function clearRect(context){
-	context.clearRect(0,0,_w,_h);
-}
-
-cas.addEventListener("mousemove",function(ev){
-	if (lock===true) {
-		var evt = ev||window.event;
-		// 获取鼠标在视口的坐标,传递参数到drawPoint
-		var moveY= evt.clientY;
-		var moveX = evt.clientX;
-		// drawPoint(context,posY,posX);
-		drawLine(context,posX,posY,moveX,moveY);
-		// 每一次的结束点变成下一次开始的点
-		posX = moveX;
-		posY = moveY;
-
-	}else{
-		return false;
-	}
-},false);
-
+// 画直线
 function drawLine(context,x1,y1,x2,y2){
 	// 保存当前绘图状态
 	context.save();
-
+	context.beginPath();
 	context.lineCap = "round";
 	context.lineWidth=radius*2;
-
-	context.beginPath();
 	// 以原点为起点,绘制一条线
 	context.moveTo(x1,y1);
 	context.lineTo(x2,y2);
 	context.stroke();
 	// 恢复原有绘图状态
 	context.restore();
-}
+}/**/
 
+/*function drawAll(context,x,y,x1,y1){
+	context.save();
+	context.beginPath();
+	if (arguments.length===3) {
+		context.arc(x,y,radius,0,2*Math.PI);
+		context.globalCompositeOperation ="destination-out";
+		// context.fillStyle="white";
+		context.fill();
+		context.save();
+	}else{
+		context.lineCap = "round";
+		context.lineWidth=radius*2;
+		// 以原点为起点,绘制一条线
+		context.moveTo(x,y);
+		context.lineTo(x1,y1);
+	}
+	context.stroke();
+	// 恢复原有绘图状态
+	context.restore();
+}*/
+// 清除画布
+function clearRect(context){
+	context.clearRect(0,0,_w,_h);
+}
 window.onload=function(){
 	drawMask(context);
 	drawLine(context);
@@ -102,7 +130,7 @@ function getTransparencyPercent(context){
 		}
 	}
 	var percent = (num/(_w*_h))*100;
-	console.log("透明个数"+num);
-	console.log("占总面积"+Math.ceil(percent)+"%");
+	// console.log("透明个数"+num);
+	// console.log("占总面积"+Math.ceil(percent)+"%");
 	return Math.round(Math.ceil(percent));
 }
